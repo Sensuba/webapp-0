303ba69609e7d5606cd5d8cd5cdd158adbaf8f83
FlynnTheInfinite <fabienmorival@gmail.com>
2018-07-31 23:53:42 +0200

    Updated css for heroes

--- src/components/cards/Spell.js
+++ src/components/cards/Spell.js
@@ -11,7 +11,13 @@ export default class Spell extends Component {
 		<img crossOrigin="Anonymous" className="sensuba-card-bg" src={this.props.src.imgLink} alt={this.props.src.nameCard}/>
 	    <div className="sensuba-card-header">
 	    	<div className="sensuba-card-mana">{this.props.src.mana}</div>
-	        <div className={"sensuba-card-title" + (this.props.src.nameCard.length >= 25 ? " sensuba-card-long-title" : "")}>{this.props.src.nameCard}</div>
+	        <div className={"sensuba-card-title" +
+	        	(this.props.src.nameCard.length >= 25 ?
+	        		(this.props.src.nameCard.length >= 30 ? " sensuba-card-very-long-title" : " sensuba-card-long-title")
+	        		: ""
+	        	)}>
+	        	{this.props.src.nameCard}
+	        </div>
 	    </div>
 	    <div className="sensuba-card-body">
 	    	<div className="sensuba-card-body-header">
