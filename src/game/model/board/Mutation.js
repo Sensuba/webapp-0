dc06f6587cfdb7b295af3fecf62ebf79ef1a433d
FlynnTheInfinite <fabienmorival@gmail.com>
2019-10-31 18:20:03 +0100

    Corrected mutations applied on unknown cards

--- src/game/model/board/Mutation.js
+++ src/game/model/board/Mutation.js
@@ -7,13 +7,15 @@ class Mutation {
 
 	attach (card) {
 
+		if (!card || !card.mutations)
+			return;
 		this.card = card;
 		this.card.mutations.push(this);
 	}
 
 	detach () {
 
-		if (!this.card)
+		if (!this.card || !this.card.mutations)
 			return;
 		this.card.mutations = this.card.mutations.filter (m => m !== this);
 		this.card = null;
