df727319604359029b979385b94a8d9c725ce4f5
Flynn <fabienmorival@gmail.com>
2020-07-06 00:10:34 +0200

    Réintégration de la musique

--- src/game/Game.js
+++ src/game/Game.js
@@ -86,8 +86,7 @@ export default class Game extends Component {
     this.sequencer = new Sequencer(this, this.state.model, this.store.dispatch);
 
     this.volume = localStorage.getItem('sound.sfx') !== undefined ? localStorage.getItem('sound.sfx') : 1;
-    //var music = Math.random() < 0.33 ? "virgocluster" : (Math.random() < 0.5 ? "planemo" : "auroraborealis");
-    var music = "void";
+    var music = Math.random() < 0.33 ? "virgocluster" : (Math.random() < 0.5 ? "planemo" : "auroraborealis");
     this.audio = new Audio("/audio/" + music + ".mp3");
     this.audio.volume = localStorage.getItem('sound.music') !== undefined ? localStorage.getItem('sound.music') * 0.2 : 0.2;
     this.mute = localStorage.getItem('sound.muted');
--- src/game/Replay.js
+++ src/game/Replay.js
@@ -44,8 +44,7 @@ export default class Replay extends Component {
 
 
     this.volume = localStorage.getItem('sound.sfx') !== undefined ? localStorage.getItem('sound.sfx') : 1;
-    //var music = Math.random() < 0.33 ? "virgocluster" : (Math.random() < 0.5 ? "planemo" : "auroraborealis");
-    var music = "void";
+    var music = Math.random() < 0.33 ? "virgocluster" : (Math.random() < 0.5 ? "planemo" : "auroraborealis");
     this.audio = new Audio("/audio/" + music + ".mp3");
     this.audio.volume = localStorage.getItem('sound.music') !== undefined ? localStorage.getItem('sound.music') * 0.2 : 0.2;
     this.mute = true;
