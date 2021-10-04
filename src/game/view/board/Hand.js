3301d3a7c37d9274837df676eb83554f69fbb72a
Flynn <fabienmorival@gmail.com>
2021-10-04 05:44:58 +0200

    Correction

--- src/game/view/board/Hand.js
+++ src/game/view/board/Hand.js
@@ -11,7 +11,7 @@ export default class Hand extends Component {
       <div className={"sensuba-hand " + (this.props.top ? "sensuba-hand-top" : "sensuba-hand-bottom")}>
       {
       	this.props.model.cards.map((model, i) => {
-      		var shift = (-4.1 - (count-1) * (5 - count/7) + i * (10 - count/3.5)) * (this.props.model.id.no === 1 ? 1 : 1.4);
+      		var shift = (-4.1 - (count-1) * (5 - count/7) + i * (10 - count/3.5)) * (this.props.top ? 1 : 1.4);
       		return <Card style={{left: "calc(50% " + (shift > 0 ? "+" : "-") + " " + Math.abs(shift) + "vw)"}} key={model.id.no} model={model} master={this.props.master} select={m => this.props.master.manager.select(m)}/>
       	})
       }
