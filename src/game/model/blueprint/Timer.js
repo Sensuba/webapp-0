75faaf135c5e08e54567ea7f2b986a15e129c078
Flynn <fabienmorival@gmail.com>
2021-10-27 12:43:55 +0200

    Ajout du cleanup

--- src/game/model/blueprint/Timer.js
+++ src/game/model/blueprint/Timer.js
@@ -7,7 +7,7 @@ class Timer extends Bloc {
 
 		super("timer", src, ctx);
 		this.f = (src, ins, props) => {
-			var unsub = src.gameboard.subscribe(ins[0].time ? "endturn" : "newturn", (t,s,d) => {
+			var unsub = src.gameboard.subscribe(this.timeToEvent(ins[0].time), (t,s,d) => {
 				if (ins[0].player === null || ins[0].player.isPlaying) {
 					if (this.callback) {
 						this.callback.execute(props);
@@ -19,6 +19,16 @@ class Timer extends Bloc {
 		}
 		this.types = [Types.timestamp];
 		this.toPrepare.push("callback");
+	}
+
+	timeToEvent (time) {
+
+		switch (time) {
+		case 0: return "newturn";
+		case 1: return "endturn";
+		case 2: return "cleanup";
+		default: return "newturn";
+		}
 	}
 }
 
--- src/game/model/blueprint/Timestamp.js
+++ src/game/model/blueprint/Timestamp.js
@@ -7,8 +7,18 @@ class Timestamp extends Bloc {
 	constructor (src, ctx) {
 
 		super("timestamp", src, ctx);
-		this.f = (src, ins) => [ new Event(src, ins[0].time ? "endturn" : "newturn", (t,s,d) => ins[0].player === null || ins[0].player.isPlaying) ];
+		this.f = (src, ins) => [ new Event(src, this.timeToEvent(ins[0].time), (t,s,d) => ins[0].player === null || (ins[0].player === 1 && src.area && src.area.isPlaying) || (ins[0].player === 2 && src.area && src.area.opposite.isPlaying)) ];
 		this.types = [Types.timestamp];
+	}
+
+	timeToEvent (time) {
+
+		switch (time) {
+		case 0: return "newturn";
+		case 1: return "endturn";
+		case 2: return "cleanup";
+		default: return "newturn";
+		}
 	}
 }
 
