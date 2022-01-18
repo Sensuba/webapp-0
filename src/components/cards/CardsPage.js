89b39d5611093fcd7c0556507190d8ca815149d7
Flynn <fabienmorival@gmail.com>
2022-01-18 22:20:27 +0100

    Ajout de la page Nouvelle Destinée

--- src/components/cards/CardsPage.js
+++ src/components/cards/CardsPage.js
@@ -372,7 +372,8 @@ export default class CardsPage extends Component {
                 <option value="4">Grand Bal Masqué</option>
                 <option value="5">Croisée des Horizons</option>
                 <option value="6">Académie Supermagique</option>
-                { (() => { if (User.isConnected() && User.getData().authorization > 3) return <option value="7">Nouvelle Destinée</option> })() }
+                <option value="7">Nouvelle Destinée</option>
+                { (() => { if (User.isConnected() && User.getData().authorization > 3) return <option value="8">A venir</option> })() }
               </select>
               <div>
                 { (nocards > 0 ? <b>{ nocards }</b> : "Aucune")}{ " carte" + (nocards > 1 ? "s" : "") + " trouvée" + (nocards > 1 ? "s" : "") }
