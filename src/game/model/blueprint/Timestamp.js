d2fff5a8355dc219156876ded9aca93b684c9bca
Flynn <fabienmorival@gmail.com>
2022-09-30 17:22:27 +0200

    Correction

--- src/game/model/blueprint/Timestamp.js
+++ src/game/model/blueprint/Timestamp.js
@@ -7,7 +7,7 @@ class Timestamp extends Bloc {
 	constructor (src, ctx) {
 
 		super("timestamp", src, ctx);
-		this.f = (src, ins) => [ new Event(src, this.timeToEvent(ins[0].time), (t,s,d) => ins[0].player === null || (ins[0].player === 0 && src.area && src.area === s) || (ins[0].player === 1 && src.area && src.area.opposite === s)) ];
+		this.f = (src, ins) => [ new Event(src, this.timeToEvent(ins[0].time), (t,s,d) => ins[0].player === null || (ins[0].player === 0 && src.area && src.area.id.no === s.no) || (ins[0].player === 1 && src.area && src.area.opposite.id.no === s.no)) ];
 		this.types = [Types.timestamp];
 	}
 
