c159f123038207896bd5bc6d8bfdc77f21b1f2ae
Fabien Morival <fabienmorival@gmail.com>
2026-01-09 12:13:39 +0100

    Invert gravity

--- src/game/model/board/Card.js
+++ src/game/model/board/Card.js
@@ -1119,10 +1119,10 @@ export default class Card {
 		if (this.states && this.states.frozen && !this.frozen)
 			this.states.frozen = false;
 		if (this.poisondmg && this.hasState("vaccinated"))
-			delete this.poisondmg;
+			delete this.poisondmg
+		this.states = this.states || {};;
 		if (this.hasState("invertgravity"))
 			this.states.flying = !this.hasState("flying");
-		this.states = this.states || {};
 		this.states.poisoned = this.poisoned;
 		this.computing = false;
 
